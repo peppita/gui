5ebf1ea1a10222f75f7765c9602a4af6815b09ac
Andreas Prummer <andreas.prummer@kununu.com>
2017-03-07 22:11:46 +0100

    Fix failing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import { createProjectStore } from './projectStore';
+
+const projectStore = createProjectStore();
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={projectStore}>
+      <App />
+    </Provider>, div);
 });
